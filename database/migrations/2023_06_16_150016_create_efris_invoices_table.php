70accb049ab0bbda7ffe32be8a352dbb1174667e
MUWONGE HASSAN <hassansaava@gmail.com>
2023-06-16 18:33:03 +0300

    refactored efris invoice migration

--- database/migrations/2023_06_16_150016_create_efris_invoices_table.php
+++ database/migrations/2023_06_16_150016_create_efris_invoices_table.php
@@ -15,28 +15,10 @@ class CreateEfrisInvoicesTable extends Migration
     {
         Schema::create('efris_invoices', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('antifake_code');
-            $table->string('currency');
-            $table->unsignedBigInteger('data_source');
-            $table->unsignedBigInteger('device_no');
-            $table->unsignedBigInteger('invoice_id');
-            $table->unsignedBigInteger('invoice_industry_code');
-            $table->unsignedBigInteger('invoice_kind');
-            $table->unsignedBigInteger('invoice_no');
-            $table->unsignedBigInteger('invoice_type');
-            $table->unsignedBigInteger('is_batch');
-            $table->unsignedBigInteger('is_invalid');
-            $table->unsignedBigInteger('is_refund');
-            $table->date('issued_date');
-            $table->date('issued_date_pdf');
-            $table->string('operator');
-            $table->string('pay_way');
+            $table->json('basicInformation');
 
             // buyerDetails
-            $table->string('buyer_address');
-            $table->string('buyer_legal_name');
-            $table->string('buyer_sector');
-            $table->unsignedBigInteger('buyer_type');
+            $table->json('buyerDetails');
 
             // extend
             $table->json('extend');
